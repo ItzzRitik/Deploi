395b9165723d95a2dbc90264ae806fac07712de4
Ritik Srivastava <ritik.space@gmail.com>
2020-06-24 03:11:03 +0530

    Initial Server Commit

---
+++ index.js
@@ -0,0 +1,34 @@
+express = require('express'),
+    app = express(),
+	http = require('http'),
+	server = http.createServer(app),
+	bodyparser = require('body-parser'),
+	ip = require('ip'),
+    chalk = require('chalk'),
+    socketIO = require('socket.io'),
+	io = socketIO(server),
+	
+	socketUtils = require('./tools/socketUtils');
+
+require('dotenv').config();
+const env = process.env;
+
+app.set('view engine', 'ejs');
+app.use('/public', express.static('public'));
+app.use('/lib', express.static('node_modules'));
+app.use(bodyparser.json({limit: '50mb'}));
+app.use(bodyparser.urlencoded({limit: '50mb', extended: true, parameterLimit:50000}));
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
+
+server.listen(env.PORT || 8080, function() {
+    logger.clear();
+    logger.log(true, 'Starting Server');
+    logger.log(false, 'Server is running at', 
+        chalk.blue('http://' + (env.IP || ip.address() || 'localhost') + ':' + (env.PORT || '8080')));
+	
+	
+});

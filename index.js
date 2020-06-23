a71688f4554901419c57bd30fbf3ce534c6f9aaf
Ritik Srivastava <ritik.space@gmail.com>
2020-06-24 04:15:00 +0530

    Add jwt middleware for netlify

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-express = require('express'),
-    app = express(),
+let express = require('express'),
+	app = express(),
 	http = require('http'),
 	server = http.createServer(app),
 	bodyparser = require('body-parser'),
 	ip = require('ip'),
-    chalk = require('chalk'),
-    socketIO = require('socket.io'),
+	chalk = require('chalk'),
+	jwt = require('jsonwebtoken'),
+	socketIO = require('socket.io'),
 	io = socketIO(server),
 	
 	//socketUtils = require('./tools/socketUtils');
@@ -18,19 +19,24 @@ const env = process.env;
 app.set('view engine', 'ejs');
 app.use('/public', express.static('public'));
 app.use('/lib', express.static('node_modules'));
-app.use(bodyparser.json({limit: '50mb'}));
-app.use(bodyparser.urlencoded({limit: '50mb', extended: true, parameterLimit:50000}));
+app.use(bodyparser.json({ limit: '50mb' }));
+app.use(bodyparser.urlencoded({ limit: '50mb', extended: true, parameterLimit:50000 }));
 app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+	next();
 });
 
-app.post('/netlify', 
-(req, res, next) => {
+let netlifySigned = (req, res, next) => {
+	let signature = req.header('X-Webhook-Signature'),
+		options = { iss: 'netlify', verify_iss: true, algorithm: 'HS256' };
+	console.log(signature);
+
+	jwt.decode(signature, env.NETLIFY_KEY, true, options);
 	next();
-}
-,(req, res) => {
+};
+
+app.post('/netlify', netlifySigned, (req, res) => {
 	console.log('Netlify Body: ', req.body);
 	res.status(200).send('');
 });
@@ -45,8 +51,8 @@ app.get('/*', (req, res) => {
 });
 
 server.listen(env.PORT || 8080, function() {
-    logger.clear();
-    logger.log(true, 'Starting Server');
-    logger.log(false, 'Server is running at', 
-        chalk.blue('http://' + (env.IP || ip.address() || 'localhost') + ':' + (env.PORT || '8080')));
+	logger.clear();
+	logger.log(true, 'Starting Server');
+	logger.log(false, 'Server is running at', 
+		chalk.blue('http://' + (env.IP || ip.address() || 'localhost') + ':' + (env.PORT || '8080')));
 });
